5f7e9be7a88753e4610d99f51105a28d1379f59d
Ezequiel Aceto <ezequiel.aceto@gmail.com>
2019-11-05 22:39:34 -0300

    Update example app

--- Example/SCPKIHelper/ViewController.swift
+++ Example/SCPKIHelper/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import SCPKIHelper
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    let spec = SCPKIKeySpec.common
+    
+    let keysId = "test_key_1"
+    
+    private var publicKey: SecKey?
+    private var privateKey: SecKey?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        _ = spec.secureInKeychain(true)
+        let authContext = LAContext()
+        authContext.localizedReason = "Access private key"
+        authContext.localizedCancelTitle = "Cancel"
+        authContext.localizedFallbackTitle = "Fallback"
+        authContext.touchIDAuthenticationAllowableReuseDuration = 60
+        
+        SCPKIHelper.shared.set(authenticationContext: authContext)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +36,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clearAll(_ sender: Any) {
+        SCPKIHelper.shared.removeKeys(identifiedBy: keysId)
+    }
+    
+    @IBAction func generateKeys(_ sender: Any) {
+        SCPKIHelper.shared.generateKeyPair(with: spec, identifiedBy: keysId) { [weak self] publicKey, privateKey, error in
+            self?.publicKey = publicKey
+            self?.privateKey = privateKey
+            debugPrint("\(publicKey)")
+            debugPrint("\(privateKey)")
+            debugPrint("\(error)")
+        }
+    }
+    
+    @IBAction func getKeys(_ sender: Any) {
+        SCPKIHelper.shared.getKeyPair(with: spec, identifiedBy:
+            keysId)  { [weak self] publicKey, privateKey, error in
+                self?.publicKey = publicKey
+                self?.privateKey = privateKey
+                debugPrint("\(publicKey)")
+                debugPrint("\(privateKey)")
+                debugPrint("\(error)")
+       }
+    }
+    
+    @IBAction func getPrivate(_ sender: Any) {
+        SCPKIHelper.shared.getPrivateKey(with: spec, identifiedBy: keysId) { privateKey, error in
+            debugPrint("\(privateKey)")
+            debugPrint("\(error)")
+        }
+    }
+    
+    @IBAction func getPublic(_ sender: Any) {
+        SCPKIHelper.shared.getPublicKey(with: spec, identifiedBy: keysId) { publicKey, error in
+            
+            debugPrint("\(publicKey)")
+            debugPrint("\(error)")
+        }
+    }
+    
+    @IBAction func getCSR(_ sender: Any) {
+
+    }
 }
 
